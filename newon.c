7a0c2c62f77d7b4b0c2155aad7b0d50f6daa94bb
vaishali232 <51689354+vaishali232@users.noreply.github.com>
2019-06-11 10:47:04 +0530

    Create newon.c

---
+++ newon.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+	int n;
+	scanf("%d",&n);
+	if(n==0)
+	{
+		printf("Zero");
+		}
+		else if(n>0)
+		{
+			printf("Positive");
+		}
+		else
+		{
+			printf("Negative");
+		}
+
+	return 0;
+}
